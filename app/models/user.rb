af3f72807eba472a2352a8b64d232732c55bacab
Tsuchinoko9 <tdkyou9@gmail.com>
2023-07-19 18:40:10 +0900

    ユーザーのカラムのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :profile, presence: true
+  validates :occupation, presence: true
+  validates :position, presence: true
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
