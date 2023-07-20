c62224dea21c805acf2c8c77a1fdb43fa4c5aca9
sawara711 <uekibati.su@gmail.com>
2023-07-20 17:43:36 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   #get 'prototypes/index'
 
-  #devise 未導入のため、コメントアウトとする
-  #devise_for :users
+  # devise 未導入のため、コメントアウトとする
+  # devise_for :users
   root to: 'prototypes#index'
   
+  # プロトタイプの中身にコメントを表示するため下記のように記載する
+  resources :prototypes do
+    resources :comments, only: :create
+  end
+
 end
