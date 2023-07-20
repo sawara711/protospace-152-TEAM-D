b8e6efcb4209a72951bad73bc214ac69db8665dd
sawara711 <uekibati.su@gmail.com>
2023-07-20 17:29:20 +0900

    テーブル作成

--- db/migrate/20230720073002_create_prototypes.rb
+++ db/migrate/20230720073002_create_prototypes.rb
@@ -1,7 +1,16 @@
 class CreatePrototypes < ActiveRecord::Migration[7.0]
   def change
     create_table :prototypes do |t|
-
+      #association
+      t.integer :user_id
+      #prototype name
+      t.string :title
+      #catch copy
+      t.text :catch_copy
+      #concept
+      t.text :concept
+      #image
+      t.text :image
       t.timestamps
     end
   end
--- db/migrate/20230720073028_create_comments.rb
+++ db/migrate/20230720073028_create_comments.rb
@@ -1,7 +1,11 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-
+      #association
+      t.integer :user_id
+      t.integer :prototype_id
+      #comment
+      t.text :text
       t.timestamps
     end
   end
