6c5e1ce6b2d325e61b350cd5202698cc3c18b06b
sawara711 <uekibati.su@gmail.com>
2023-07-20 16:42:03 +0900

    モデル・コントローラー作成・ルートのルーティング設定・pry-rails追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  #get 'prototypes/index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  #devise 未導入のため、コメントアウトとする
+  #devise_for :users
+  root to: 'prototypes#index'
+  
 end
