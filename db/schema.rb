90222bd7b2f3ba454434c370156234167940ae05
sawara711 <uekibati.su@gmail.com>
2023-07-21 14:07:09 +0900

    不明の変更（恐らくmigrateによるもの）

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_19_082212) do
-  create_table "users", charset: "utf8", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "name", null: false
-    t.text "profile", null: false
-    t.text "occupation", null: false
-    t.text "position", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
 ActiveRecord::Schema[7.0].define(version: 2023_07_20_073028) do
   create_table "comments", charset: "utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -43,6 +27,22 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_20_073028) do
     t.text "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", charset: "utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "name", null: false
+    t.text "profile", null: false
+    t.text "occupation", null: false
+    t.text "position", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
