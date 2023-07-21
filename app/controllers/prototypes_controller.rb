486e20d5f5d5794d7e8d39aadb18577581f3acb3
sawara711 <uekibati.su@gmail.com>
2023-07-21 16:12:25 +0900

    createアクション設定

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,5 @@
 class PrototypesController < ApplicationController
+  before_action :move_to_index, except: [:index ]
 
   def index
   end
@@ -7,9 +8,35 @@ class PrototypesController < ApplicationController
   end
   
   def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(create_params_setting)
+    if @prototype.valid?
+      @prototype.save
+      redirect_to root_path
+    else
+      @prototype[:image] = nil
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  private
+  def keep_params
+    params.require(:prototype).permit( :title, :catch_copy, :concept,)
+  end
+  def create_params_setting
+    params.require(:prototype).permit( :title, :catch_copy, :concept, :image ).merge(user_id: current_user.id)
   end
   
-  def show
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 
 end
