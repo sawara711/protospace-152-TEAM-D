8a62ed9dd998d718f157886aef671435a67171bf
sawara711 <uekibati.su@gmail.com>
2023-07-21 12:58:33 +0900

    微修正（スペース・コメントアウト削除など）

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   validates :occupation, presence: true
   validates :position, presence: true
 
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,6 @@ Rails.application.routes.draw do
   #get 'prototypes/index'
   root to: "prototypes#index"
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-
-  # devise 未導入のため、コメントアウトとする
-  # devise_for :users
-  root to: 'prototypes#index'
   
   # プロトタイプの中身にコメントを表示するため下記のように記載する
   resources :prototypes do
