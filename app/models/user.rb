21169dbeed210afab5889f2a84cd53c5b5e1dbef
Tsuchinoko9 <tdkyou9@gmail.com>
2023-07-24 11:05:23 +0900

    ユーザーのアソシエーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :prototypes
+  has_many :comments
+
 end
