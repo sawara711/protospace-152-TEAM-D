c571492662d59dc66362a169e1b90aa50082cfbb
sawara711 <uekibati.su@gmail.com>
2023-07-24 12:09:00 +0900

    ログイン有無確認

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :move_to_index, except: [:index ]
+  before_action :move_to_index, except: [:index, :show ]
 
   def index
     @prototypes = Prototype.includes(:user)
